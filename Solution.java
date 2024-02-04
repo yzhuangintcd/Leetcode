cc302fc53c90a2e79468dc1ef3926aedf9aae69f
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-02-04 12:42:10 +0000

    Added solution for sequentialDigits

--- Solution.java
+++ Solution.java
@@ -38,13 +38,36 @@ class Solution {
         }
         return answer;
     }
-    
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> list = new LinkedList<>();
+        int arr[] = {12, 23, 34, 45, 56, 67, 78, 89,
+            123, 234, 345, 456, 567, 678, 789,
+            1234, 2345, 3456, 4567, 5678, 6789, 
+            12345, 23456, 34567, 45678, 56789, 
+            123456, 234567, 345678, 456789,
+            1234567, 2345678, 3456789,
+            12345678, 23456789,
+            123456789};
+
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] >= low && arr[i] <= high){
+                list.add(arr[i]);
+            }
+            if (arr[i] > high){
+                break;
+            }
+        }
+        return list;
+    }
+
     public static void main(String [] args){
         Solution tester = new Solution();
-        int [] arr = {1,2,2,4};
-        for (int i= 0; i < tester.findErrorNums(arr).length; i++ ){
-            System.out.print(tester.findErrorNums(arr)[i] + ", "); 
-        }
+
+        // int [] arr = {1,2,2,4};
+        // for (int i= 0; i < tester.findErrorNums(arr).length; i++ ){
+        //     System.out.print(tester.findErrorNums(arr)[i] + ", "); 
+        // }
         
+        System.out.println("Test sequential digits: " + tester.sequentialDigits(1000, 13000));
     }
 }
