1d8912013a95310bd75189f176af0285c01616a1
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-02-05 21:09:18 +0000

    Leetcode 387. First unique character in a string solution added

--- Solution.java
+++ Solution.java
@@ -38,6 +38,8 @@ class Solution {
         }
         return answer;
     }
+
+    // this is NOT a good solution
     public List<Integer> sequentialDigits(int low, int high) {
         List<Integer> list = new LinkedList<>();
         int arr[] = {12, 23, 34, 45, 56, 67, 78, 89,
@@ -60,6 +62,56 @@ class Solution {
         return list;
     }
 
+    
+    public int firstUniqChar(String s) {
+        // use a stack - not a good idea
+        int answer = -1;
+        
+        // make a stringbuilder and use sub arrays?
+        // how about we add the unique letter into the stringbuilder
+        // like we can:
+        // - put the first char into a variable, 
+        // - iterate through the string for search for the char 
+        // - if doesn't find it 
+        // -- add it to the sb
+        // - if it finds it
+        // -- skip to the next letter
+
+        // if the sb is empty at the end, it means there is no unique char 
+        // - return -1
+        // if the sb is non-empty then we can take the first letter of sb
+        // return the index of that letter on the inputted string
+
+        // the logic above works but exceeded the time limit 
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++){
+            char temp = s.charAt(i);
+            count = 0;
+            for (int j = 0; j < s.length();j++){
+                char temp2 = s.charAt(j);
+                if (temp == temp2){
+                    count++;
+                }
+                if (count > 1){
+                    break;
+                }
+            }
+            if (count > 1){
+                continue;
+            }
+            sb.append(temp);
+        }
+
+        if (sb.isEmpty()){
+            return -1;
+        }
+
+        char ans = sb.charAt(0);
+        answer = s.indexOf(ans);
+        return answer;
+    }
+    
     public static void main(String [] args){
         Solution tester = new Solution();
 
@@ -68,6 +120,6 @@ class Solution {
         //     System.out.print(tester.findErrorNums(arr)[i] + ", "); 
         // }
         
-        System.out.println("Test sequential digits: " + tester.sequentialDigits(1000, 13000));
+        System.out.println(tester.firstUniqChar("loveleetcode"));
     }
 }
