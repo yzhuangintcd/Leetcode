46e1efa0797fddd30d56001ea74e971cd8d5f1ab
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-02-13 17:24:51 +0000

    Add Solution: firstPalindrome

--- Solution.java
+++ Solution.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import javax.swing.*;
 
 class Solution {
     public int climbStairs(int n) {
@@ -152,6 +151,25 @@ class Solution {
         }
         return answer;
     }
+    
+    public String firstPalindrome(String[] words) {
+        for (int i = 0; i < words.length; i++){
+            if (isPalindrome(words[i])){
+                return words[i];
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome (String word){
+        if (word.length() == 1 || word.length() == 0) return true;
+        for (int i = 0, j = word.length() - 1; i < word.length()/2; i++, j--){
+            if (word.charAt(i) != word.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Solution tester = new Solution();
@@ -164,6 +182,7 @@ class Solution {
 
         // System.out.println(tester.firstUniqChar("loveleetcode"));
 
-        System.out.println(tester.majorityElement(arr));
+        String [] s = {"abc","car","ada","racecar","cool"};
+        System.out.println(tester.firstPalindrome(s));
     }
 }
