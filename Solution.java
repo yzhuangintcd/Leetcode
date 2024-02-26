01a1d6b3087549af739ea1ea6d23a5c0d235c4a3
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-02-26 23:58:11 +0000

    added solution for isSameTree

--- Solution.java
+++ Solution.java
@@ -286,6 +286,65 @@ class Solution {
         System.out.println();
     }
 
+    public class TreeNode 
+    {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) return true;
+        if (p == null && q != null) return false;
+        if (p != null && q == null) return false;
+        
+        StringBuilder p1 = new StringBuilder();
+        StringBuilder q1 = new StringBuilder();
+
+        treeToString(p, p1);
+        treeToString(q, q1);
+
+        System.out.println(p1);
+        System.out.println(q1);
+
+        if (p1.compareTo(q1) == 0){
+            return true;
+        }
+        return false;
+    }
+
+    // this function appends the p in inorder traversal ontpo s
+    private void treeToString(TreeNode p, StringBuilder s){
+        if (p == null){
+            s.append("null");
+            
+        }
+        else {
+            s.append(p.val);
+        }
+
+        if (p.left != null) {
+            treeToString(p.left, s);
+        }
+        else {
+            s.append("null");
+            
+        }
+        
+        if (p.right != null) {
+            treeToString(p.right, s);
+        }else {
+            s.append("null");
+        }
+    }
 
     public static void main(String[] args) {
         Solution tester = new Solution();
@@ -298,6 +357,8 @@ class Solution {
         
 
         System.out.println(tester.missingNumber(arr));
+
+        
     }
 }
 
