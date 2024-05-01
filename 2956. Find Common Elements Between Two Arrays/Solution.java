236efdba9c1b4fbc3321f50529115129977a9173
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-05-01 11:40:57 +0100

    Added Solution 2956

---
+++ 2956. Find Common Elements Between Two Arrays/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        int[] answer = new int[2]; // Return an integer array answer of size 2 containing the two values...
+        int index = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            int temp = nums1[i];
+            for (int j = 0; j < nums2.length; j++) {
+                if (temp == nums2[j]) {
+                    count1++;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            int temp = nums2[i];
+            for (int j = 0; j < nums1.length; j++) {
+                if (temp == nums1[j]) {
+                    count2++;
+                    break;
+                }
+            }
+        }
+        answer[0] = count1;
+        answer[1] = count2;
+        return answer;
+    }
+}
--- Solution.java
+++ Solution.java
@@ -37,10 +37,40 @@ class Solution {
         System.out.println();
     }
 
-    
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        int[] answer = new int[2]; // Return an integer array answer of size 2 containing the two values...
+        int index = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            int temp = nums1[i];
+            for (int j = 0; j < nums2.length; j++) {
+                if (temp == nums2[j]) {
+                    count1++;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            int temp = nums2[i];
+            for (int j = 0; j < nums1.length; j++) {
+                if (temp == nums1[j]) {
+                    count2++;
+                    break;
+                }
+            }
+        }
+        answer[0] = count1;
+        answer[1] = count2;
+        return answer;
+    }
 
     public static void main(String[] args) {
         Solution tester = new Solution();
-        
+        int [] arr1 = {3,4,2,3};
+        int [] arr2 = {1,5};
+
+        int [] intersection = tester.findIntersectionValues(arr1, arr2);
+        tester.printArray(intersection);
     }
 }
