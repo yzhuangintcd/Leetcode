ca7822055001529701af08dc54801eef5a818c14
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-05-10 14:57:54 +0100

    Complete Problem Number of Student Unable to eat Lunch 1700

---
+++ 1700. Number of Students Unable to Eat Lunch/countStudents.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class countStudents {
+    public static Queue<Integer> arrayToQueue(int[] array) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int num : array) {
+            queue.offer(num);
+        }
+        return queue;
+    }
+
+    public int countStudents(int[] students, int[] sandwiches) {
+
+        Queue<Integer> studentQueue = arrayToQueue(students);
+        Queue<Integer> sandwichQueue = arrayToQueue(sandwiches);
+
+        // if the studentQueue peek == sandwichQueue peek
+        // remove both head of the queue
+        int rep = 0;
+        int count = 0;
+        while (!studentQueue.isEmpty()) {
+
+            if (studentQueue.peek() == sandwichQueue.peek()) {
+                studentQueue.remove(studentQueue.peek());
+                sandwichQueue.remove(sandwichQueue.peek());
+                count = 0;
+                rep++;
+            } else {
+                studentQueue.offer(studentQueue.peek());
+                studentQueue.remove(studentQueue.peek());
+                count++;
+            }
+
+            if (count > students.length) {
+                return students.length - rep;
+            }
+        }
+        return 0;
+    }
+}
--- Solution.java
+++ Solution.java
@@ -3,8 +3,6 @@ import java.util.*;
 public class Solution {
 
 
-
-
     // Helper Function to print out arrays of int
     public void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -15,5 +13,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution tester = new Solution();
+        int [] arr1 = {1,1,1,0,0,1};
+        int [] arr2 = {1,0,0,0,1,1};
+        
     }
 }
