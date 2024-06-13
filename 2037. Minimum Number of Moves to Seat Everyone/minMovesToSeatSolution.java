3612863ccea9f105e1aaaa282c3ad2da909891d7
yzhuangintcd <127947054+yzhuangintcd@users.noreply.github.com>
2024-06-13 11:53:45 +0100

    Added Solution for 2037

--- 2037. Minimum Number of Moves to Seat Everyone/minMovesToSeatSolution.java
+++ 2037. Minimum Number of Moves to Seat Everyone/minMovesToSeatSolution.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 
-public class Solution {
+public class minMovesToSeatSolution {
     public int minMovesToSeat(int[] seats, int[] students) {
         int minMoves = 0;
         // ArrayList<Integer> seatList = new ArrayList<>();
@@ -39,31 +35,4 @@ public class Solution {
         // }
         return minMoves;
     }
-
-    // Helper Function to print out array of int
-    public void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-
-    // Helper Function to print out 2d array of int
-    public void printGrid(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                System.out.print(grid[i] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] seats = { 3,1,5 };
-        int[] students = { 2,7,4 };
-        System.out.println(s.minMovesToSeat(seats, students));
-    }
-
 }
